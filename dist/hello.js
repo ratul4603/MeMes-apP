eb0beb1cf27cfb721e3ab79640718c55bfc78f22
Md Ratul Anjum <136952957+ratul4603@users.noreply.github.com>
2023-10-08 19:48:11 +0600

    Add files via upload

---
+++ dist/hello.js
@@ -0,0 +1,17 @@
+"use strict";
+const card = document.querySelector(".card");
+const getMeme = () => {
+    fetch('https://meme-api.com/gimme')
+        .then((res) => res.json())
+        .then((data) => {
+        console.log(data);
+        card.innerHTML = `
+            <img src="${data.url}" alt="">
+            <p>${data.title}</p>
+            <button id="btn">Get Meme</button>
+        `;
+        const button = document.getElementById("btn");
+        button.addEventListener("click", getMeme);
+    });
+};
+window.addEventListener("load", getMeme);
